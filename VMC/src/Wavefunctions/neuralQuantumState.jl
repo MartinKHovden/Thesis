066e400826bed293f3819aea9c1f7e3ce51263fa
martinkh <marthvdn@online.no>
2021-04-21 23:55:43 +0200

    Work on neural network wavefunction

--- VMC/src/Wavefunctions/neuralQuantumState.jl
+++ VMC/src/Wavefunctions/neuralQuantumState.jl
@@ -45,25 +45,47 @@ function testComputePsi()
     println(computePsi(nn, x))
 end
 
-function nnComputeGradient(nnqs, loss, x)
+function nnComputeParameterGradient(nnqs, loss, x)
+    println("Params", params(nnqs.model))
     grads = gradient(params(nnqs.model)) do 
         loss(x)
     end 
     return grads
 end 
 
-function testComputeGradient()
+function testComputeParameterGradient()
     numDims = 3
     numParticles = 2
-    numHidden = 10
+    numHidden = 100
     x = randn(numDims*numParticles)
     nn = NNQS(Chain(Dense(numParticles*numDims, numHidden), Dense(numHidden, 1)))
     loss(x) = sum(nn.model(x))
-    println(nnComputeGradient(nn, loss, x))
+    println(nnComputeParameterGradient(nn, loss, x))
+    return 0
 
 end
 
+function nnComputeGradient(nnqs, loss, x)
+    # println(x)
+    grads = gradient(Params([x])) do 
+        loss(x)
+    end 
+    # println(grads.grads)
+    return grads
+end
+
+function nnTestComputeGradient()
+    numDims = 2
+    numParticles = 1
+    numHidden = 2
+    x = randn(numDims*numParticles)
+    nn = NNQS(Chain(Dense(numParticles*numDims, numHidden), Dense(numHidden, 1)))
+    loss(x) = sum(nn.model(x))
+    println("testComputeGradient ", nnComputeGradient(nn, loss, x)[x])
+end
+
 function nnComputeLaplacian()
+    # println("Hessian = ", Zygote.hessian(loss, x))
     return 0
 end
 
@@ -71,5 +93,12 @@ function nnComputeParameterGradient()
     return 0
 end 
 
-testComputePsi()
-testComputeGradient()
+# @time testComputePsi()
+# @time testComputeParameterGradient()
+@time nnTestComputeGradient()
+@time nnTestComputeGradient()
+@time nnTestComputeGradient()
+
+# @time nnTestComputeLaplacian()
+# @time nnTestComputeLaplacian()
+
