e695153fa29773da3cb064dfd87b18ce115bb4a2
martinkh <marthvdn@online.no>
2021-04-22 15:32:21 +0200

    Update initializeSystem.jl

--- VMC/src/initializeSystem.jl
+++ VMC/src/initializeSystem.jl
@@ -1,15 +1,55 @@
 module initializeSystem 
 
 struct slaterJastrow
+    particles::Array{Float64, 2}
+    n_particles::Int64 
+    n_dims::Int64
 
+    alpha::Float64 
+    omega::Float64
+    beta::Float64
+
+    slaterMatrixSpinUp::Array{Float64, 2}
+    slaterMatrixSpinDown::Array{Float64, 2}
+
+    inverseSlaterMatrixSpinUp::Array{Float64, 2}
+    inverseSlaterMatrixSpinDown::Array{Float64, 2}
 end 
 
 struct slaterRBM 
-    
+    particles::Array{Float64, 2}
+    n_particles::Int64 
+    n_dims::Int64
+
+    alpha::Float64 
+    omega::Float64
+    beta::Float64
+
+    slaterMatrixSpinUp::Array{Float64, 2}
+    slaterMatrixSpinDown::Array{Float64, 2}
+
+    inverseSlaterMatrixSpinUp::Array{Float64, 2}
+    inverseSlaterMatrixSpinDown::Array{Float64, 2}
+
+    nqs::NQS
 end 
 
 struct slaterNN 
+    particles::Array{Float64, 2}
+    n_particles::Int64 
+    n_dims::Int64
 
+    alpha::Float64 
+    omega::Float64
+    beta::Float64
+
+    slaterMatrixSpinUp::Array{Float64, 2}
+    slaterMatrixSpinDown::Array{Float64, 2}
+
+    inverseSlaterMatrixSpinUp::Array{Float64, 2}
+    inverseSlaterMatrixSpinDown::Array{Float64, 2}
+
+    nn::NN
 end 
 
 function initializeSlater()
